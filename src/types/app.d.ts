28bf93ec9f143d63e22102048e01b5cb19b5c710
MohamadAlgazli <brawjym@gmail.com>
2025-05-03 20:21:02 +0300

    update portfolio start commit

--- src/types/app.d.ts
+++ src/types/app.d.ts
@@ -1,37 +1,34 @@
-import React from "react";
-
-
 export interface Basic {
-    name: string;
-    intro: string;
-    about: string;
-    github: string;
+  name: string;
+  intro: string;
+  about: string;
+  github: string;
 }
 
 export interface Skill {
-    id: number;
-    title: string;
-    description: string;
-    icon: React.ReactNode;
+  id: number;
+  title: string;
+  description: string;
+  icon: React.ReactNode;
 }
 
 export interface Project {
-    id: number;
-    name: string;
-    description: React.ReactNode;
-    repository: string;
-    images: string[];
-    technologies: Technology[];
+  id: number;
+  name: string;
+  description: React.ReactNode;
+  repository: string;
+  images: string[];
+  technologies: Technology[];
 }
 
 export interface Technology {
-    name: string;
-    description: string;
-    image: string;
+  name: string;
+  description: string;
+  image: string;
 }
 
 export interface SocialLink {
-    name: string;
-    href: string;
-    icon: React.ReactNode;
+  name: string;
+  href: string;
+  icon: React.ReactNode;
 }
