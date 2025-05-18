872358fe4d70bd923982a21763e156846bb19d95
MohamadAlgazli <brawjym@gmail.com>
2025-05-18 15:31:16 +0300

    edit animation in contact section

--- src/sections/contact/index.tsx
+++ src/sections/contact/index.tsx
@@ -2,6 +2,7 @@ import Container from "../../components/ui/container";
 import Header from "../../components/ui/header";
 import ContactSocialLinks from "./contact-social-links";
 import ContactForm from "./contact-form";
+import { motion } from "motion/react";
 
 const Contact = () => {
   return (
@@ -10,14 +11,17 @@ const Contact = () => {
       section
       id="contact-section"
     >
-      <Header>Contact</Header>
-      <div
+      <Header animation>Contact</Header>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ once: true }}
         className="flex sm:flex-row flex-col sm:gap-8"
       >
         <ContactForm />
         <hr className="sm:hidden my-8 border-2 border-background-foreground/25" />
         <ContactSocialLinks />
-      </div>
+      </motion.div>
     </Container>
   );
 };
